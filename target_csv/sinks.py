cf6da1c8f314627206f109528cafedd47c46a366
jnorris3 <jnorris3@buffalo.edu>
2025-04-18 14:44:32 -0400

    test

--- target_csv/sinks.py
+++ target_csv/sinks.py
@@ -99,7 +99,7 @@ class CSVSink(BatchSink):
             records = sorted(records, key=lambda x: x[sort_property_name])
 
         self.logger.info(f"Writing {len(context['records'])} records to file...")
-        self.logger.info(f"record count: {self.config}")
+        self.logger.info(f"record count: {target.state}")
         
 
         write_csv(
